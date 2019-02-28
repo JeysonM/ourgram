43c2c60d80a4d0e8f4eb83747da9b6a9661e37e5
Jeyson Mirabal <jeyson.mirabal@gmail.com>
2019-02-28 11:18:04 -0400

    Primer activity - CreateAccountActivity

---
+++ app/src/main/java/cbb/bo/edu/ourgram/view/CreateAccountActivity.java
@@ -0,0 +1,15 @@
+package cbb.bo.edu.ourgram.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import cbb.bo.edu.ourgram.R;
+
+public class CreateAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+    }
+}
